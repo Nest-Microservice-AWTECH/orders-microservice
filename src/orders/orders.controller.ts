7c70823da64d5cd1f998b22fbeb8cec17c5e855c
Alfonso Fuentes <fuentesw3008@gmail.com>
2025-01-16 22:03:59 -0500

    OrdesMS

---
+++ src/orders/orders.controller.ts
@@ -0,0 +1,24 @@
+import { Controller, ParseIntPipe, ParseUUIDPipe } from '@nestjs/common';
+import { MessagePattern, Payload } from '@nestjs/microservices';
+import { OrdersService } from './orders.service';
+import { CreateOrderDto } from './dto/create-order.dto';
+
+@Controller()
+export class OrdersController {
+  constructor(private readonly ordersService: OrdersService) {}
+
+  @MessagePattern('createOrder')
+  create(@Payload() createOrderDto: CreateOrderDto) {
+    return this.ordersService.create(createOrderDto);
+  }
+
+  @MessagePattern('findAllOrders')
+  findAll() {
+    return this.ordersService.findAll();
+  }
+
+  @MessagePattern('findOneOrder')
+  findOne(@Payload('id', ParseUUIDPipe) id: string) {
+    return this.ordersService.findOne(id);
+  }
+}
